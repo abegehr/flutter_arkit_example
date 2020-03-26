36f0337e0af611a9450845fcdefd311c9ba45ef6
Anton Begehr <a.begehr@gmail.com>
2020-03-26 08:59:33 +0100

    fix tab controller length

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
-        length: 3,
+        length: 4,
         child: Scaffold(
           appBar: AppBar(
             bottom: TabBar(
